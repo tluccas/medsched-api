1de7713fea6e2fe2422ecf25bbb9f3ccfd3fe154
Lucas Alves <lucas1901alves@gmail.com>
2026-02-07 03:15:17 -0300

    feat(user): add user and role entities

---
+++ src/main/java/com/alvesdev/medsched_api/domain/model/Role.java
@@ -0,0 +1,29 @@
+package com.alvesdev.medsched_api.domain.model;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Table(name = "roles")
+@Data
+public class Role {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    @ManyToMany(mappedBy = "roles")
+    private Set<User> users = new HashSet<>();
+}
