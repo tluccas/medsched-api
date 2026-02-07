5a82c6d979aa1aa7780d4f005623ccc1f0e98d79
Lucas Alves <lucas1901alves@gmail.com>
2026-02-07 00:17:47 -0300

    feat(structure): add initial project structure

---
+++ src/main/java/com/alvesdev/medsched_api/MedschedApiApplication.java
@@ -0,0 +1,13 @@
+package com.alvesdev.medsched_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedschedApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedschedApiApplication.class, args);
+	}
+
+}
