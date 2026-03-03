b5616dd0903b211f59b4487417744a9ec68dd5d8
Lucas Alves <lucas1901alves@gmail.com>
2026-03-03 15:26:49 -0300

    refactor(profile): refactor doctorDetailResponse to improve security

--- src/main/java/com/alvesdev/medsched_api/dto/response/user/DoctorDetailResponse.java
+++ src/main/java/com/alvesdev/medsched_api/dto/response/user/DoctorDetailResponse.java
@@ -2,8 +2,7 @@ package com.alvesdev.medsched_api.dto.response.user;
 
 import java.util.UUID;
 
-public record DoctorDetailResDto(
-    UUID userId,
+public record DoctorDetailResponse(
     UUID doctorId,
     String name,
     String specialization
