fecbb423ff1f91251bf4e37f10395337a51982ab
Lucas Alves <lucas1901alves@gmail.com>
2026-03-03 17:42:16 -0300

    feat(patient): add update and findbyId methods

--- src/main/java/com/alvesdev/medsched_api/controllers/PatientController.java
+++ src/main/java/com/alvesdev/medsched_api/controllers/PatientController.java
@@ -1,6 +1,7 @@
 package com.alvesdev.medsched_api.controllers;
 
 import java.util.List;
+import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -8,24 +9,58 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.alvesdev.medsched_api.domain.services.PatientService;
-import com.alvesdev.medsched_api.dto.response.user.PatientDetailResDto;
+import com.alvesdev.medsched_api.dto.request.profiles.UpdatePatientRequest;
+import com.alvesdev.medsched_api.dto.response.user.PatientDetailResponse;
 
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import jakarta.validation.Valid;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PathVariable;
+
 
 
 @Controller
-@RequestMapping("/patients")
+@RequestMapping("/users/patients")
 public class PatientController {
     @Autowired
     private PatientService patientService;
 
     @Operation(summary = "Get all patients", description = "Returns a list of all patients in the system.")
     @GetMapping("")
-    public ResponseEntity<List<PatientDetailResDto>> getAllPatients() {
+    public ResponseEntity<List<PatientDetailResponse>> getAllPatients() {
         return ResponseEntity.ok(patientService.getAllPatients());
+    }
+
+    @Operation(summary = "Get patient by User ID", description = "Returns the details of a patient by their unique identifier.")
+    @ApiResponses(
+        {
+            @ApiResponse(responseCode = "200", description = "Patient found and returned successfully."),
+            @ApiResponse(responseCode = "404", description = "Patient not found with the provided ID."),
+            @ApiResponse(responseCode = "400", description = "Invalid request parameters.")
+        }
+    )
+    @GetMapping("/{uuid}")
+    public ResponseEntity<PatientDetailResponse> getPatientById(@PathVariable UUID uuid) {
+        return ResponseEntity.ok(patientService.getByProfileId(uuid));
+    }
+
+    @Operation(summary = "Update patient details", description = "Updates the details of a patient by their unique identifier.")
+    @ApiResponses(
+        {
+            @ApiResponse(responseCode = "200", description = "Patient details updated successfully."),
+            @ApiResponse(responseCode = "404", description = "Patient not found with the provided ID."),
+            @ApiResponse(responseCode = "400", description = "Invalid request data.")
+        }
+    )
+    @PutMapping("/{uuid}")
+    public ResponseEntity<PatientDetailResponse> updatePatient(@PathVariable UUID uuid, @RequestBody @Valid UpdatePatientRequest dto) {
+        PatientDetailResponse updatedPatient = patientService.update(uuid, dto);
+        return ResponseEntity.ok(updatedPatient);
     }
     
 }
--- src/main/java/com/alvesdev/medsched_api/domain/services/PatientService.java
+++ src/main/java/com/alvesdev/medsched_api/domain/services/PatientService.java
@@ -1,21 +1,34 @@
 package com.alvesdev.medsched_api.domain.services;
 
 import java.util.List;
+import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
+
+import com.alvesdev.medsched_api.exceptions.UserNotFoundException;
+
+import jakarta.transaction.Transactional;
+
 import org.springframework.stereotype.Service;
 
+import com.alvesdev.medsched_api.domain.model.PatientProfile;
+import com.alvesdev.medsched_api.domain.model.User;
 import com.alvesdev.medsched_api.domain.repositories.PatientRepository;
-import com.alvesdev.medsched_api.dto.response.user.PatientDetailResDto;
+import com.alvesdev.medsched_api.dto.response.user.PatientDetailResponse;
+import com.alvesdev.medsched_api.domain.repositories.UserRepository;
+import com.alvesdev.medsched_api.dto.request.profiles.UpdatePatientRequest;
 
 @Service
 public class PatientService {
     @Autowired
     PatientRepository patientRepository;
 
-    public List<PatientDetailResDto> getAllPatients() {
+    @Autowired
+    UserRepository userRepository;
+
+    public List<PatientDetailResponse> getAllPatients() {
         return patientRepository.findAll().stream()
-            .map(patient -> new PatientDetailResDto(
+            .map(patient -> new PatientDetailResponse(
                 patient.getUser().getId(),
                 patient.getId(),
                 patient.getUser().getUsername(),
@@ -24,4 +37,49 @@ public class PatientService {
             ))
             .toList();
     }
+
+    public PatientDetailResponse getByProfileId(UUID uuid) {
+        User user = userRepository.findById(uuid)
+                .orElseThrow(() -> new UserNotFoundException("This user does not exist."));
+        
+        PatientProfile patient = user.getPatientProfile();
+
+        if (patient == null) {
+            throw new UserNotFoundException("This user does not have a patient profile.");
+        }
+
+        return new PatientDetailResponse(
+            patient.getUser().getId(),
+            patient.getId(),
+            patient.getUser().getUsername(),
+            patient.getBirthDate(),
+            patient.getMedicalHistory()
+        );
+    }
+
+    @Transactional
+    public PatientDetailResponse update(UUID uuid, UpdatePatientRequest dto){
+        User user = userRepository.findById(uuid)
+                .orElseThrow(() -> new UserNotFoundException("This user does not exist."));
+        
+        PatientProfile patient = user.getPatientProfile();
+
+        if (patient == null) {
+            throw new UserNotFoundException("This user does not have a patient profile.");
+        }
+
+        patient.setBirthDate(dto.birthDate());
+        patient.setMedicalHistory(dto.medicalHistory());
+
+        PatientProfile updatedPatient = patientRepository.save(patient);
+
+        return new PatientDetailResponse(
+            updatedPatient.getUser().getId(),
+            updatedPatient.getId(),
+            updatedPatient.getUser().getUsername(),
+            updatedPatient.getBirthDate(),
+            updatedPatient.getMedicalHistory()
+        );
+    }
+
 }
