fa7e54c053b834ff490ab34725ba8fdb10595f45
Lucas Alves <lucas1901alves@gmail.com>
2026-03-09 23:06:41 -0300

    fix(appointment): fix schedule relationship

--- src/main/java/com/alvesdev/medsched_api/domain/model/Appointment.java
+++ src/main/java/com/alvesdev/medsched_api/domain/model/Appointment.java
@@ -17,7 +17,6 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import lombok.Data;
 import lombok.NoArgsConstructor;
